9c94d9fafac1e319354a6215d5e5cbc7ccf45d0d
Timur Vafin <timur.vafin@flatstack.com>
2013-03-21 12:50:54 +0400

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails3BaseApi::Application.routes.draw do
+  # Devise
+  #
   devise_for :users, only: []
   devise_scope :user do
     post 'users/sign_in', to: 'sessions#create'
   end
 
+  # Resources require authenticated user
+  #
   namespace :user do
     resources :comments, only: [:index]
   end
 
-  resources :posts, only: [:index, :show] do
-    resources :comments,
-      only: [:index],
-      controller: :post_comments
-  end
+  # Public resources
+  #
+  resources :posts, only: [:index, :show]
 
-  resources :comments, only: [:index, :show]
-
-  root :to => 'comments#index'
+  root :to => 'posts#index'
 
   mount ApiTaster::Engine => '/api_taster' if defined? ApiTaster::Engine
 end
