e1105bbd6eeac753829874d6b60fab8fb1c29220
Vasily Polovnyov <vasily@polovnyov.ru>
2013-09-24 18:09:38 +0400

    s/Rails3BaseApi/RailsBaseApi

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails3BaseApi::Application.configure do
+RailsBaseApi::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
