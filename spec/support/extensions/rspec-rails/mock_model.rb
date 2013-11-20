87d484a70b3aea794ac7b0e942915f2ab3d7c71a
Timur Vafin <timur.vafin@flatstack.com>
2013-11-20 17:40:14 +0400

    Shorten line

--- spec/support/extensions/rspec-rails/mock_model.rb
+++ spec/support/extensions/rspec-rails/mock_model.rb
@@ -1,12 +1,14 @@
-module RSpec
-  module Rails
-    module Mocks
-      def mock_model_with_read_attribute_for_serialization(string_or_model_class, stubs = {})
-        stubs = stubs.update(read_attribute_for_serialization: 'attribute')
-        mock_model_without_read_attribute_for_serialization(string_or_model_class, stubs)
-      end
+module RSpec::Rails::Mocks
+  def mock_model_with_read_attribute_for_serialization(string_or_model_class, \
+    stubs = {})
 
-      alias_method_chain :mock_model, :read_attribute_for_serialization
-    end
+    stubs = stubs.update(read_attribute_for_serialization: 'attribute')
+
+    mock_model_without_read_attribute_for_serialization(
+      string_or_model_class,
+      stubs
+    )
   end
+
+  alias_method_chain :mock_model, :read_attribute_for_serialization
 end
