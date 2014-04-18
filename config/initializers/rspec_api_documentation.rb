49d58acced1209364adf45f83dc3e8e9b9baafca
Timur Vafin <timur.vafin@flatstack.com>
2014-04-18 20:41:05 +0400

    Update rspec_api_documentation configuration
    - filter usefulness headers
    - add CURL output
    - sort examples how they listed in the specs

--- config/initializers/rspec_api_documentation.rb
+++ config/initializers/rspec_api_documentation.rb
@@ -1,4 +1,8 @@
 RspecApiDocumentation.configure do |config|
   config.format = :json
   config.docs_dir = Rails.root.join('doc', 'api')
+  config.request_headers_to_include = ['Accept']
+  config.curl_host = "http://#{app_config.host}"
+  config.curl_headers_to_filter = ['Cookie', 'Host', 'Content-Type']
+  config.keep_source_order = true
 end if defined? RspecApiDocumentation
