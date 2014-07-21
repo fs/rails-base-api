b18b9d461d6acec807c133cfef1ae3f1b8bb36e9
Ildar Kayumov <ildar.kayumov@flatstack.com>
2014-07-21 18:16:19 +0400

    Separate posts and comments seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,4 +1,1 @@
-user = FactoryGirl.create :user
-posts = FactoryGirl.create_list :post, 5
-post = posts.first
-comments = FactoryGirl.create_list :comment, 2, post: post, user: user
+FactoryGirl.create :user
