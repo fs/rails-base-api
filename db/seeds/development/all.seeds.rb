f4b77ec19ac5a3efde946d45ebc0e9dd6f869cc4
Ildar Kayumov <ildar.kayumov@flatstack.com>
2014-07-21 11:41:35 +0400

    Add posts and comments to seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,1 +1,4 @@
-FactoryGirl.create :user
+user = FactoryGirl.create :user
+posts = FactoryGirl.create_list :post, 5
+post = posts.first
+comments = FactoryGirl.create_list :comment, 2, post: post, user: user
