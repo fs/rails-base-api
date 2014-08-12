b79af2d980fed6d77e4d92e42f7ec918e69f4ef7
Ildar Kayumov <ildar.kayumov@flatstack.com>
2014-08-12 11:15:06 +0400

    Use simple array on collection serializer instead users

--- spec/serializers/collection_serializer_spec.rb
+++ spec/serializers/collection_serializer_spec.rb
@@ -1,35 +1,33 @@
 require 'rails_helper'
 
 describe CollectionSerializer do
-  let(:users) { build_list :user, 3 }
+  let(:collection) { [1, 2, 3] }
 
-  let(:json) { CollectionSerializer.new(users, root: :users).to_json }
-  let(:users_json) { parse_json(json) }
+  let(:serialized_collection) { CollectionSerializer.new(collection) }
+  let(:serialized_object) { serialized_collection.object }
+  let(:serialized_meta) { serialized_collection.options[:meta] }
 
   let(:meta) do
     {
-      'pagination' =>
+      pagination:
       {
-        'total' => 3,
-        'per_page' => 25,
-        'page' => 1
+        total: 3,
+        per_page: 25,
+        page: 1
       }
     }
   end
 
   before do
-    allow(users).to receive_messages(
+    allow(collection).to receive_messages(
       total_count: 3,
       limit_value: 25,
       current_page: 1
     )
   end
 
-  it 'returns correct users' do
-    users_json['users'].each_with_index do |user_json, index|
-      expect(user_json).to be_a_user_representation(users[index])
-    end
-
-    expect(users_json['meta']).to be_a_meta_representation(meta)
+  it 'returns collection with meta' do
+    expect(serialized_object).to eql(collection)
+    expect(serialized_meta).to be_a_meta_representation(meta)
   end
 end
--- spec/support/matchers/be_a_meta_representation.rb
+++ spec/support/matchers/be_a_meta_representation.rb
@@ -1,6 +1,6 @@
-RSpec::Matchers.define :be_a_meta_representation do |meta|
-  match do |json|
-    expect(json).to be
-    expect(json).to eql(meta)
+RSpec::Matchers.define :be_a_meta_representation do |expected|
+  match do |meta|
+    expect(meta).to be
+    expect(meta).to eql(expected)
   end
 end
