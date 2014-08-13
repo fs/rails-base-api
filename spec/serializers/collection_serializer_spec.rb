27185af292e115e54eb2ce00cd3ce0bf570ab5f3
Ildar Kayumov <ildar.kayumov@flatstack.com>
2014-08-13 11:18:58 +0400

    Check hash with meta on collection serializer instead of using custom matchers

--- spec/serializers/collection_serializer_spec.rb
+++ spec/serializers/collection_serializer_spec.rb
@@ -1,33 +1,23 @@
 require 'rails_helper'
 
 describe CollectionSerializer do
-  let(:collection) { [1, 2, 3] }
-
-  let(:serialized_collection) { CollectionSerializer.new(collection) }
-  let(:serialized_object) { serialized_collection.object }
-  let(:serialized_meta) { serialized_collection.options[:meta] }
-
-  let(:meta) do
-    {
-      pagination:
-      {
-        total: 3,
-        per_page: 25,
-        page: 1
-      }
-    }
-  end
-
-  before do
-    allow(collection).to receive_messages(
-      total_count: 3,
-      limit_value: 25,
-      current_page: 1
-    )
-  end
+  let(:collection) { Kaminari.paginate_array([1, 2, 3]).page(1) }
+  let(:serialized_collection) { CollectionSerializer.new(collection, root: :collection).as_json }
 
   it 'returns collection with meta' do
-    expect(serialized_object).to eql(collection)
-    expect(serialized_meta).to be_a_meta_representation(meta)
+    expect(serialized_collection).to eql(
+      {
+        collection: [1, 2, 3],
+        meta:
+        {
+          pagination:
+          {
+            total: 3,
+            per_page: 25,
+            page: 1
+          }
+        }
+      }
+    )
   end
 end
--- spec/support/matchers/be_a_meta_representation.rb
+++
@@ -1,6 +0,0 @@
-RSpec::Matchers.define :be_a_meta_representation do |expected|
-  match do |meta|
-    expect(meta).to be
-    expect(meta).to eql(expected)
-  end
-end
