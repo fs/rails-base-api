f44cd89928e75c272d557236b8a268e2a5145eef
Timur Vafin <timur.vafin@flatstack.com>
2014-08-21 14:35:05 +0400

    Remove unused let from posts_spec

--- spec/acceptance/v1/posts_spec.rb
+++ spec/acceptance/v1/posts_spec.rb
@@ -3,9 +3,7 @@ require 'rspec_api_documentation/dsl'
 
 resource 'Posts' do
   let!(:posts) { create_list :post, 2 }
-  let(:post) { posts.first }
-  let!(:comment) { create :comment, post: post }
-  let!(:id) { post.id }
+  let(:first_post) { posts.first }
 
   subject(:response) { json_response_body }
 
@@ -16,14 +14,16 @@ resource 'Posts' do
     let(:params) { { page: 1, per_page: 25 } }
 
     example_request 'Listing posts' do
-      expect(response['posts'].first).to be_a_post_representation(post)
+      expect(response['posts'].first).to be_a_post_representation(first_post)
       expect(response['meta']).to be_a_meta_representation_of(posts, params)
     end
   end
 
   get '/v1/posts/:id' do
+    let(:id) { first_post.id }
+
     example_request 'Single post' do
-      expect(response['post']).to be_a_post_representation(post)
+      expect(response['post']).to be_a_post_representation(first_post)
     end
   end
 end
