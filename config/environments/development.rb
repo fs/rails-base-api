9c5c9df8010fd5783cbf9492c3bb86ed3c5bd364
Anton Fefilov <anton.fefilov@flatstack.com>
2015-05-22 10:43:26 +0400

    Apply review

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Open letters in browser
+  # Preview email in the browser instead of sending it.
   config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
