724c5a40673ff245975909ece7b891c44401e9dd
spodlecki <spodlecki@skintertainment.com>
2015-07-07 10:14:08 -0500

    rubocop passing

--- app/serializers/paginated_array_serializer.rb
+++ app/serializers/paginated_array_serializer.rb
@@ -1,13 +1,13 @@
 class PaginatedArraySerializer < ActiveModel::ArraySerializer
   def initialize(object, options = {})
     options[:meta] ||= {}
-    options[:meta].merge!({
+    options[:meta].merge!(
       pagination: {
         total: object.total_count,
         per_page: object.limit_value,
         page: object.current_page
       }
-    })
+    )
 
     super(object, options)
   end
