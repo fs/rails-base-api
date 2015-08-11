70013f095178afc955234a6d0fad553082160aed
Timur Vafin <timur.vafin@flatstack.com>
2015-08-11 15:06:53 +0300

    Use update interactor syntax.
    * app/controllers/sessions_controller was removed b/c it was moved to v1 namespace in previous commits.
    * use #call instead of #perform on AuthenticateUser interactor class

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ module V1
     skip_before_action :skip_trackable
 
     def create
-      user = AuthenticateUser.perform(warden: warden).user
+      user = AuthenticateUser.call(warden: warden).user
       respond_with(user)
     end
   end
