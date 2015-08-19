b9c989f73cd6cab49eca2c2c50d5ea1420d38505
Vasily Polovnyov <vasily@polovnyov.ru>
2015-08-19 18:39:47 +0300

    rip off secret_key and pepper from devise conf
    Also:
      * drop initializers/secret_token.rb as it's been replaced by secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = "2bcc1492fb9e2f8158a1bb5e951022317eba080b195d676d77d65f2f8a2ee751d816683e05a8ea21673219"
-Rails.application.config.secret_key_base = "376a9c237816fff5b0fc1a0febd3a7929e4f114e04b6d1179fe8c85edaa0e4d492d6fadcca7c73f9543a7a"
