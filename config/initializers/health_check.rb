8dafa251646e9492bdf8fd79c2af17e83b7d2d55
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2016-07-25 17:07:03 +0300

    Remove unnecessary empty lines

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -8,13 +8,11 @@ HealthCheck.setup do |config|
   # http status code used when plain text error message is output
   # Set to 200 if you want your want to distinguish between partial (text does not include success) and
   # total failure of rails application (http status of 500 etc)
-
   config.http_status_for_error_text = 500
 
   # http status code used when an error object is output (json or xml)
   # Set to 200 if you want your want to distinguish between partial (healthy property == false) and
   # total failure of rails application (http status of 500 etc)
-
   config.http_status_for_error_object = 500
 
   # You can customize which checks happen on a standard health check
