f1e8c2e60688aae881736dcd08b07851c23b4c6d
AlexeyKuznetzov <alexey.kuznetzov@flatstack.com>
2016-10-05 23:32:24 +0300

    Update RSpec API docs gem

--- config/initializers/rspec_api_documentation.rb
+++ config/initializers/rspec_api_documentation.rb
@@ -3,7 +3,7 @@ RspecApiDocumentation.configure do |config|
   config.docs_dir = Rails.root.join("doc", "api", "v1")
   config.request_headers_to_include = %w(Accept X-User-Token X-User-Email Content-Type)
   config.response_headers_to_include = %w(Content-Type)
-  config.curl_host = "https://timebox-api.herokuapp.com"
+  config.curl_host = "https://#{ENV.fetch('HOST')}"
   config.curl_headers_to_filter = %w(Cookie Host Origin)
   config.request_body_formatter = :json
   config.keep_source_order = true
