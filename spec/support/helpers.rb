39ffc33380d154984385c37ec0f278fe0c1246fc
AlexeyKuznetzov <alexey.kuznetzov@flatstack.com>
2016-10-08 11:49:43 +0300

    Add JSONAPI acceptance test helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,6 +4,32 @@ module Helpers
   end
 
   def json_response_body
-    parse_json(response_body)
+    parse_json response_body
+  end
+
+  def response_data
+    response["data"]
+  end
+
+  def response_attrs
+    return response_data["attributes"] unless response_data.is_a?(Array)
+
+    response_data.map { |d| d["attributes"] }
+  end
+
+  def response_status_code
+    Rack::Utils::HTTP_STATUS_CODES[response_status].parameterize("_").to_sym
+  end
+
+  def jsonapi_params(type, attributes, id = nil)
+    params = {
+      data: {
+        type: type,
+        attributes: attributes
+      }
+    }
+
+    params[:data][:id] = id if id
+    params
   end
 end
