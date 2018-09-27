4157c4302787aa96114dfeb17100112a2463bd2d
eduardryazapov <eduard.ryazapov@flatstack.com>
2018-09-27 17:12:51 +0300

    remove interactor examples

--- spec/support/shared_examples/failure_interactor.rb
+++
@@ -1,7 +0,0 @@
-shared_examples "failure interactor" do |params|
-  it "fails" do
-    interactor.run
-    expect(context).to be_failure
-    expect(context.message).to eql(params[:message]) if params
-  end
-end
